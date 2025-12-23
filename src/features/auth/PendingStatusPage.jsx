ad506645f234cbf697d93394e2cbc40411b5a2df
Aymon777 <119906045+Aymona777@users.noreply.github.com>
2025-12-23 18:39:17 +0200

    pending &student profile edit

--- src/features/auth/PendingStatusPage.jsx
+++ src/features/auth/PendingStatusPage.jsx
@@ -1,3 +1,4 @@
+//eslint-disable jsx-a11y/alt-text
 import React, { useEffect, useState } from "react";
 import { authStorage } from "../../core/auth/auth.storage";
 import { apiFetch } from "../../core/api/http";
